7b0aad0bb96bd1f5183ec83d480ac4183d4566fa
Rodolfoba1 <38680428+Rodolfoba1@users.noreply.github.com>
2022-06-21 11:29:48 -0600

    interface

--- device.py
+++ device.py
@@ -1,3 +1,25 @@
 from abc import ABCMeta, abstractmethod
 
+
 class Device(metaclass=ABCMeta):
+    @abstractmethod
+    def select_tasks(self) -> bool:
+        raise NotImplementedError
+
+    def enable(self):
+        raise NotImplementedError
+
+    def disable(self):
+        raise NotImplementedError
+
+    def get_volume(self):
+        raise NotImplementedError
+
+    def set_volumne(self, percent: float):
+        raise NotImplementedError
+
+    def get_channel(self):
+        raise NotImplementedError
+
+    def set_channel(self, channel: int):
+        raise NotImplementedError

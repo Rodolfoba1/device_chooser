e2877860c1483f83f78504bcdfd06c41ef01da13
Rodolfoba1 <38680428+Rodolfoba1@users.noreply.github.com>
2022-06-21 11:37:02 -0600

    interface2

--- device.py
+++ device.py
@@ -6,20 +6,26 @@ class Device(metaclass=ABCMeta):
     def select_tasks(self) -> bool:
         raise NotImplementedError
 
+    @abstractmethod
     def enable(self):
         raise NotImplementedError
 
+    @abstractmethod
     def disable(self):
         raise NotImplementedError
 
+    @abstractmethod
     def get_volume(self):
         raise NotImplementedError
 
+    @abstractmethod
     def set_volumne(self, percent: float):
         raise NotImplementedError
 
+    @abstractmethod
     def get_channel(self):
         raise NotImplementedError
 
+    @abstractmethod
     def set_channel(self, channel: int):
         raise NotImplementedError
